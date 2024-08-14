01f4693494beba9cde7130d1e277d6bf747a96e5
estebanstack <130319203+estebanstack@users.noreply.github.com>
2024-08-14 07:14:41 -0500

    Update Fact.java

--- src/Factorial/Fact.java
+++ src/Factorial/Fact.java
@@ -1,17 +1,27 @@
 package Factorial;
 
-public class Fact {
-
+public class Main {
     public static void main(String[] args) {
-        int n=5;
-        System.out.println(factorial(n));
+        System.out.println(Suma(1,4));
+        System.out.println(Multiplicacion(2,4));
+    }
+    public static int Sucesora (int n){
+        return n+1;
     }
 
-    public static int factorial(int n){
-        if(n==0||n==1){
-            return 1;
-        } else{
-            return n*factorial(n-1);
+    public static int Suma (int x, int y){
+        int resultado=x;
+        for (int i = 0; i < y; i++) {
+            resultado=Sucesora(resultado);
         }
+        return resultado;
+    }
+
+    public static int Multiplicacion (int x, int y){
+        int resultado=0;
+        for (int i = 0; i < y; i++) {
+            resultado=Suma(resultado,x);
+        }
+        return resultado;
     }
 }
